42a128d900b6c0734c8cbc2c24d628378d8a3e71
immunity428 <pega921sus@gmail.com>
2024-12-18 21:10:14 +0900

    feat:課題機能の実装

---
+++ access-log/test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# ログファイルのパス
+log_file="test.log"
+
+echo "アクセス回数が多いIPアドレス Top5"
+
+awk '{print $1}' "$log_file" |
+    uniq -c | sort -nr | head -n 5 |
+    awk '{
+    rank = NR; # 現在の行番号を順位として取得
+    printf "%d位: %-15s 回数: %d\n", rank, $2, $1
+}'
+
+# uniq -c 出現回数をカウントし、カウント数を行の先頭に出力
+# sort -nr n:数値順, r:降順　で並べ替え
+# awk 一言で言えばC言語にできる
+# head -n 5 Top5のみ表示
+# %-15 15 文字分の幅で表示
+# awk '{print $1}' 最初のフィールド（$1）を抽出
