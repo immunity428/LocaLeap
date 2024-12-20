8cc9298850cd40b2800f07bb478a2fe826942541
immunity428 <pega921sus@gmail.com>
2024-12-20 16:25:46 +0900

    feat:エラーメッセージの追加

--- access-log/Analysis.sh
+++ access-log/Analysis.sh
@@ -2,19 +2,26 @@
 
 # ログファイルのパス
 log_file="access.log"
+log_path="files/$log_file"
 
-echo "アクセス回数が多いIPアドレス Top5"
+# ファイル存在確認
+if [ ! -f "$log_path" ]; then #-f パスが通常のファイルであるかどうか
+    echo "エラー: ログファイルが見つかりません: $log_path"
+    exit 1 #終了
+fi
 
-awk '{print $1}' "$log_file" |
+# ファイルが空か確認 #-s ファイルサイズの確認
+if [ ! -s "$log_path" ]; then
+    echo "エラー: ログファイルが空です: $log_path"
+    exit 1 #終了
+fi
+
+echo "アクセス回数が多いIPアドレス Top5件"
+
+# IPアドレスの集計とランキング表示
+awk '{print $1}' "$log_path" |
     uniq -c | sort -nr | head -n 5 |
     awk '{
     rank = NR; # 現在の行番号を順位として取得
     printf "%d位: %-15s 回数: %d\n", rank, $2, $1
 }'
-
-# uniq -c 出現回数をカウントし、カウント数を行の先頭に出力
-# sort -nr n:数値順, r:降順　で並べ替え
-# awk 一言で言えばC言語にできる
-# head -n 5 Top5のみ表示
-# %-15 15 文字分の幅で表示
-# awk '{print $1}' 最初のフィールド（$1）を抽出
